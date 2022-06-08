4adbb9a253655a02588d42fb7197b4c2e04586f8
mago960806 <mago960806@hotmail.com>
2022-06-08 22:44:33 +0800

    feat(config): 增加了 DATABASE_URI 配置项, 默认使用 sqlite3 数据库, 同时可以调整 SQL_DEBUG 来开关原生 SQL 语句的打印

--- app/config.py
+++ app/config.py
@@ -6,6 +6,9 @@ class Settings(BaseSettings):
     SERVER_HOST: str = "localhost"
     SERVER_PORT: int = 5000
 
+    DATABASE_URI: str = "sqlite:///db.sqlite3"
+    SQL_DEBUG: bool = False
+
     class Config:
         case_sensitive = True
         env_file = ".env"
