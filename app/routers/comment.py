4f734679c7951b8e9a9a6b0a2e76d3c0f536cf07
mago960806 <mago960806@hotmail.com>
2022-06-10 17:19:25 +0800

    style(comment): 修复接口函数命名不规范的问题, 将 get_comment 改为 get_comments, 统一了依赖的注释风格

--- app/routers/comment.py
+++ app/routers/comment.py
@@ -8,7 +8,7 @@ from app.domain.comment import CommentDoesNotExistError
 from app.infrastructure.comment import CommentRepository
 from app.infrastructure.database import get_session
 from app.usecase.comment import CommentCommandUseCase
-from app.usecase.comment import CommentCreateModel, CommentReadModel
+from app.usecase.comment import CommentCreateDTO, CommentReadDTO
 from app.usecase.comment.usecase import CommentQueryUseCase
 
 api = APIRouter()
@@ -16,7 +16,7 @@ api = APIRouter()
 
 def comment_command_usecase(session: Session = Depends(get_session)) -> CommentCommandUseCase:
     """
-    创建 CommentCommandUseCase 依赖
+    创建 Comment Command UseCase 依赖
     """
     repository: CommentRepository = CommentRepository(session)
     return CommentCommandUseCase(repository)
@@ -24,26 +24,26 @@ def comment_command_usecase(session: Session = Depends(get_session)) -> CommentC
 
 def comment_query_usecase(session: Session = Depends(get_session)) -> CommentQueryUseCase:
     """
-    创建 CommentQueryUseCase 依赖
+    创建 Comment Query UseCase 依赖
     """
     repository: CommentRepository = CommentRepository(session)
     return CommentQueryUseCase(repository)
 
 
-@api.get("/comments", response_model=List[CommentReadModel])
-async def get_comment(
+@api.get("/comments", response_model=List[CommentReadDTO])
+async def get_comments(
     query_usecase: CommentQueryUseCase = Depends(comment_query_usecase),
 ):
     try:
         comments = query_usecase.fetch_all()
     except Exception as e:
         logger.info(f"查询所有评论失败: {e}")
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"查询所有评论失败: {e}")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"查询所有评论失败")
     else:
         return comments
 
 
-@api.get("/comments/{comment_id}", response_model=CommentReadModel)
+@api.get("/comments/{comment_id}", response_model=CommentReadDTO)
 async def get_comment(
     comment_id: int,
     query_usecase: CommentQueryUseCase = Depends(comment_query_usecase),
@@ -54,25 +54,25 @@ async def get_comment(
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=e.message)
     except Exception as e:
         logger.info(f"查询评论失败: {e}")
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"查询评论失败: {e}")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"查询评论失败")
     else:
         return comment
 
 
 @api.post(
     "/comments",
-    response_model=CommentReadModel,
+    response_model=CommentReadDTO,
     status_code=status.HTTP_201_CREATED,
 )
 async def create_comment(
-    data: CommentCreateModel,
+    data: CommentCreateDTO,
     command_usecase: CommentCommandUseCase = Depends(comment_command_usecase),
 ):
     try:
         comment = command_usecase.create_comment(data)
     except Exception as e:
         logger.info(f"创建评论失败: {e}")
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="评论失败")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="创建评论失败")
     else:
         return comment
 
@@ -86,5 +86,5 @@ async def delete_comment(
         command_usecase.delete_comment(comment_id)
     except Exception as e:
         logger.info(f"删除评论失败: {e}")
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="评论不存在")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="删除评论失败")
     return Response(status_code=status.HTTP_204_NO_CONTENT)
