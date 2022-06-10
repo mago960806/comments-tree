da08829e2e1809da482f7aaa3a009d9a521af5c2
mago960806 <mago960806@hotmail.com>
2022-06-10 17:10:48 +0800

    feat(user): 添加了 UserIsAlreadyExistsError 来传递用户存在的异常

--- app/domain/user/exception.py
+++ app/domain/user/exception.py
@@ -3,3 +3,10 @@ class UserDoesNotExistError(Exception):
 
     def __str__(self):
         return UserDoesNotExistError.message
+
+
+class UserIsAlreadyExistsError(Exception):
+    message = "该用户已存在"
+
+    def __str__(self):
+        return UserIsAlreadyExistsError.message
