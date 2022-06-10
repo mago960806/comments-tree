9251d21469d3aecc1a22af44f6983a836645735d
mago960806 <mago960806@hotmail.com>
2022-06-10 10:40:10 +0800

    feat(comment): 实现了 CommentRepository 的三个方法, find, save, remove, save方法中的 Update 部分还未完成

--- app/infrastructure/comment/repository.py
+++ app/infrastructure/comment/repository.py
@@ -4,34 +4,46 @@ from sqlalchemy.exc import NoResultFound
 from sqlalchemy.orm.session import Session
 
 from app.domain.comment import CommentBaseRepository, Comment
-from app.infrastructure.comment.dto import CommentDTO
+from app.infrastructure.comment.do import CommentDO
 
 
 class CommentRepository(CommentBaseRepository):
+    """
+    Comment Repository 实现
+    """
+
     def __init__(self, session: Session):
         self.session: Session = session
 
-    def create(self, comment: Comment):
-        comment_dto = CommentDTO.from_entity(comment)
+    def find(self, comment_id: int) -> Optional[Comment]:
         try:
-            self.session.add(comment_dto)
-        except:
-            self.session.rollback()
-            raise
-        else:
-            self.session.commit()
-            return comment_dto.to_entity()
-
-    def find_by_id(self, comment_id: int) -> Optional[Comment]:
-        try:
-            comment_dto: CommentDTO = self.session.query(CommentDTO).filter_by(id=comment_id).one()
+            comment_do: CommentDO = self.session.query(CommentDO).filter_by(id=comment_id).one()
         except NoResultFound:
             return None
         else:
-            return comment_dto.to_entity()
+            return comment_do.to_entity()
 
-    def update(self, comment: Comment):
-        raise NotImplementedError
+    def save(self, comment: Comment) -> Optional[Comment]:
+        if not comment.id:
+            # Create
+            comment_do = CommentDO.from_entity(comment)
+            try:
+                self.session.add(comment_do)
+            except:
+                self.session.rollback()
+                raise
+            else:
+                self.session.commit()
+                return comment_do.to_entity()
+        else:
+            # Update
+            pass
 
-    def delete_by_id(self, comment_id: int):
-        raise NotImplementedError
+    def remove(self, comment_id: int):
+        try:
+            comment_do: CommentDO = self.session.query(CommentDO).filter_by(id=comment_id).one()
+        except NoResultFound:
+            return None
+        else:
+            self.session.delete(comment_do)
+            self.session.commit()
