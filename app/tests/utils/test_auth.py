3b5889003a098e8177107739c21f677328827a63
mago960806 <mago960806@hotmail.com>
2022-06-12 23:00:36 +0800

    refactor(auth): 删除了 get_authorization_from_headers 的单元测试

--- app/tests/utils/test_auth.py
+++ app/tests/utils/test_auth.py
@@ -1,8 +1,7 @@
 import random
 import string
 
-from fastapi import Request
-from app.utils import verify_password, encrypt_password, get_authorization_from_headers
+from app.utils import verify_password, encrypt_password
 
 special_characters = r" !\"  # $%&'()*+,-./:;<=>?@[\]^_`{|}~"
 allow_characters = string.ascii_letters + string.digits + special_characters
@@ -12,7 +11,3 @@ def test_verify_password():
     plain_password = "".join([random.choice(allow_characters) for _ in range(16)])
     encrypted_password = encrypt_password(plain_password)
     assert verify_password(plain_password=plain_password, encrypted_password=encrypted_password)
-
-
-def test_get_authorization_from_headers():
-    pass
