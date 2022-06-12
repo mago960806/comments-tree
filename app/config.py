4027aeb35af910f7812ff1a81a65651064b64479
mago960806 <mago960806@hotmail.com>
2022-06-12 16:47:45 +0800

    feat(config): 添加了 SECRET_KEY 配置用于存储密钥, 添加 ACCESS_TOKEN_EXPIRE_MINUTES 用于配置 JWT 过期时间, 默认1个月

--- app/config.py
+++ app/config.py
@@ -5,9 +5,12 @@ class Settings(BaseSettings):
     DEBUG: bool = False
     SERVER_HOST: str = "localhost"
     SERVER_PORT: int = 5000
-
+    # 数据库配置
     DATABASE_URI: str = "sqlite:///db.sqlite3"
     SQL_DEBUG: bool = False
+    # 认证配置
+    SECRET_KEY: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 30  # 一个月
 
     class Config:
         case_sensitive = True
