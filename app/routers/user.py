b9febedf69d870f3563887ab7cf62b4538b04201
mago960806 <mago960806@hotmail.com>
2022-06-12 16:49:59 +0800

    feat(user): 添加了 get_current_user 和 get_current_superuser 依赖, 用于接口权限控制, 添加 login 和 get_me 接口

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,14 +1,20 @@
-from typing import List
+from typing import List, Dict
 
 from fastapi import APIRouter, status, Depends, HTTPException, Response
+from jose import jwt
 from loguru import logger
 from sqlalchemy.orm.session import Session
 
+from app.config import settings
+from app.domain import User
 from app.domain.user import UserDoesNotExistError, UserIsAlreadyExistsError
+from app.domain.user.exception import AuthenticateError
 from app.infrastructure.database import get_session
 from app.infrastructure.user import UserRepository
 from app.usecase.user import UserQueryUseCase, UserCommandUseCase
 from app.usecase.user import UserRegisterDTO, UserCreateDTO, UserReadDTO
+from app.usecase.user.dto.query import JWTTokenDTO, UserLoginDTO
+from app.utils import get_authorization_from_headers, create_access_token
 
 api = APIRouter()
 
@@ -29,11 +35,52 @@ def user_query_usecase(session: Session = Depends(get_session)) -> UserQueryUseC
     return UserQueryUseCase(repository)
 
 
-@api.post("/register")
-async def register_user(
+def get_current_user(
+    token: str = Depends(get_authorization_from_headers),
+    query_usecase: UserQueryUseCase = Depends(user_query_usecase),
+) -> UserReadDTO:
+    try:
+        payload: Dict[str, str] = jwt.decode(token, settings.SECRET_KEY)
+    except jwt.JWTError:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Token已失效")
+    try:
+        user = query_usecase.fetch_one(user_id=int(payload.get("sub")))
+    except UserDoesNotExistError as e:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=e.message)
+    if not user.is_active:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="用户未激活")
+    return user
+
+
+def get_current_superuser(
+    current_user: UserReadDTO = Depends(get_current_user),
+) -> UserReadDTO:
+    if not current_user.is_superuser:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="当前用户权限不足")
+    return current_user
+
+
+@api.post("/login", response_model=JWTTokenDTO)
+async def login(data: UserLoginDTO, query_usecase: UserQueryUseCase = Depends(user_query_usecase)):
+    try:
+        user = query_usecase.authenticate(username=data.username, email=data.email, plain_password=data.password)
+    except (UserDoesNotExistError, AuthenticateError) as e:
+        # 避免用户名爆破攻击, 不能直接返回真实原因
+        logger.info(f"用户登录失败: 用户名={data.username}, 原因={e.message}")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="用户名或密码不正确")
+    except Exception as e:
+        logger.error(f"系统错误: {e}")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="系统错误")
+    else:
+        access_token = create_access_token(subject=str(user.id))
+        return {"token": access_token}
+
+
+@api.post("/register", status_code=status.HTTP_201_CREATED, response_model=UserReadDTO)
+async def register(
     data: UserRegisterDTO,
     command_usecase: UserCommandUseCase = Depends(user_command_usecase),
-):
+) -> UserReadDTO:
     try:
         user = command_usecase.register_user(data)
     except UserIsAlreadyExistsError as e:
@@ -45,10 +92,18 @@ async def register_user(
         return user
 
 
+@api.get("/users/me", response_model=UserReadDTO)
+async def get_me(
+    current_user: User = Depends(get_current_user),
+):
+    return current_user
+
+
 @api.get("/users", response_model=List[UserReadDTO])
 async def get_users(
     query_usecase: UserQueryUseCase = Depends(user_query_usecase),
-):
+    current_user: UserReadDTO = Depends(get_current_superuser),
+) -> List[UserReadDTO]:
     try:
         users = query_usecase.fetch_all()
     except Exception as e:
@@ -62,6 +117,7 @@ async def get_users(
 async def get_user(
     user_id: int,
     query_usecase: UserQueryUseCase = Depends(user_query_usecase),
+    current_user: UserReadDTO = Depends(get_current_superuser),
 ):
     try:
         user = query_usecase.fetch_one(user_id)
@@ -82,6 +138,7 @@ async def get_user(
 async def create_user(
     data: UserCreateDTO,
     command_usecase: UserCommandUseCase = Depends(user_command_usecase),
+    current_user: UserReadDTO = Depends(get_current_superuser),
 ):
     try:
         user = command_usecase.create_user(data)
@@ -98,6 +155,7 @@ async def create_user(
 async def delete_user(
     user_id: int,
     command_usecase: UserCommandUseCase = Depends(user_command_usecase),
+    current_user: UserReadDTO = Depends(get_current_superuser),
 ):
     try:
         command_usecase.delete_user(user_id)
